2d7349fc0fb4d3ac12953280911d47e9183d1018
Nathan Hull <Nathanhull222@gmail.com>
2019-02-17 19:18:51 -0500

    Added design doc, program now scans passed documents, stems them, and adds them to the vocab set

--- prog.py
+++ prog.py
@@ -1,1 +1,39 @@
-print hello
+import sys
+from nltk.stem import SnowballStemmer as Stemmer
+
+if len(sys.argv) != 3:
+	print('Usage error: prog.py [training data file] [test data file]')
+	exit
+
+# Hold all distinct words between the files
+vocabulary = set()
+
+# NLTK stemmer to normalize words
+stemmer = Stemmer('english')
+
+print('Opening training data file')
+with open(sys.argv[1]) as f:
+	trainlines = f.readlines()
+
+print('%i lines of training data found' % len(trainlines))
+for line in trainlines:
+	words = line.split()
+	subject = words[0]
+	for word in words:
+		if len(word) > 2:
+			word = stemmer.stem(word)
+			vocabulary.add(word)
+
+print('Opening test data file')
+with open(sys.argv[2]) as f:
+	testlines = f.readlines()
+
+print('%i lines of test data found' % len(testlines))
+for line in testlines:
+	words = line.split()
+	for word in words:
+		if len(word) > 2:
+			word = stemmer.stem(word)
+			vocabulary.add(word)
+
+print('%i distinct words total found' % len(vocabulary))
